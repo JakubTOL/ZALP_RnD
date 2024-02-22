762b0d405d8075cf64c62bd021ff60c4f3240b25
Jakub Adamczyk <63499596+JakubTOL@users.noreply.github.com>
2024-02-22 19:43:44 +0100

    Add files via upload

---
+++ zalp/infrastructure/__init__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+# ----------------------------------------------------------------------------------------------------------------------
+# Author:  Mateusz Dionizy
+# E-mail:  mateusz.dionizy@zf.com
+# Company: ZF Automotive Systems Poland Sp. z o.o.
+# Created: 2023-01-01
+# ----------------------------------------------------------------------------------------------------------------------
+# This  copyrighted  document is the property of ZF Friedrichshafen AG and its affiliates, including ZF Group Automotive
+# Holdings Corp. and its subsidiary companies  ("ZF").  This document and the information contained  herein is disclosed
+# in  confidence  and  may  not be  copied, disclosed to others, or  used  for any  purpose  unless  authorized  by  ZF.
+# This  document must  be returned  to  ZF upon request and may not  be  stored in  any  database  or retrieval  system.
+#                                             © ZF Friedrichshafen AG 2022.
+# ----------------------------------------------------------------------------------------------------------------------
